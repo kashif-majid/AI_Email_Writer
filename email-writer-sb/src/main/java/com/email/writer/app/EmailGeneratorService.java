0f1478d37743b847c5b60e41e0b88608978811fd
KASHIF MAJID <53035827+kashif-majid@users.noreply.github.com>
2025-09-05 01:32:44 +0530

    Add files via upload

---
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,82 @@
+package com.email.writer.app;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@Service
+public class EmailGeneratorService {
+
+
+    private final WebClient webClient;
+    
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+    
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String generateEmailReply(EmailRequest emailRequest){
+        //Build the prompt
+        String prompt = buildPrompt(emailRequest);
+
+        //Craft a Request
+        Map<String, Object> requestBody = Map.of(
+            "contents", new Object[]{ 
+                    Map.of("parts", new Object[]{
+                        Map.of("text", prompt)
+                    })
+            }
+        );
+
+        //Do Request and get response
+        String response = webClient.post()
+            .uri(geminiApiUrl)
+            .header("Content-Type", "application/json")
+            .header("X-goog-api-key", geminiApiKey)
+            .bodyValue(requestBody)
+            .retrieve()
+            .bodyToMono(String.class)
+            .block();
+
+        // Extract Response and Return response
+        return extractResponse(response);
+    }
+
+    private String extractResponse(String response) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(response);
+            return root
+                .path("candidates").get(0)
+                .path("content")
+                .path("parts").get(0)
+                .path("text")
+                .asText();
+
+        } catch(Exception e){
+            return "Error processing request: " + e.getMessage();
+        }
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line  ");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+        }
+        prompt.append("\nHere is the email Content: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+
+}
